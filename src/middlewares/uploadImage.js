16d05cbe72d4ebd951041e1b1242aabd3f2ab526
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-09-11 17:59:49 +0700

    delete fitur: upload image to folder public

--- src/middlewares/uploadImage.js
+++ src/middlewares/uploadImage.js
@@ -3,9 +3,9 @@ const multer = require("multer");
 
 const multerUpload = multer({
 	storage: multer.diskStorage({
-		destination: function (req, file, cb) {
-			cb(null, "./public");
-		},
+		// destination: function (req, file, cb) {
+		// 	cb(null, "./public");
+		// },
 		filename: function (req, file, cb) {
 			const ext = path.extname(file.originalname);
 			const fileName = `${Date.now()}${ext}`;
